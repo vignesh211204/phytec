4a4f4ecc78d9c42687a3e7e66eb6d577fbd20060
Vigneshwaran M <146105906+vignesh211204@users.noreply.github.com>
2025-06-30 18:52:57 +0100

    Update

--- week1/Chat App/server.c
+++ week1/Chat App/server.c
@@ -1,46 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <string.h>
 #include <arpa/inet.h>
 
-#define PORT 8080
-#define BUFLEN 512
+#define BUFFER_SIZE 1024
 
-int main() {
-    int sockfd;
-    struct sockaddr_in server_addr, client_addr;
-    char buffer[BUFLEN];
-    socklen_t addr_len = sizeof(client_addr);
+int sockfd;
+struct sockaddr_in cliaddr;
+socklen_t len = sizeof(cliaddr);
 
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-    memset(&server_addr, 0, sizeof(server_addr));
-    memset(&client_addr, 0, sizeof(client_addr));
-
-    server_addr.sin_family = AF_INET; // IPv4
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("bind failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("UDP Chat Server started on port %d\n", PORT);
-
+void* receive_messages(void* arg) {
+    char buffer[BUFFER_SIZE];
     while (1) {
-        int n = recvfrom(sockfd, (char *)buffer, BUFLEN, 0, (struct sockaddr *) &client_addr, &addr_len);
+        int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&cliaddr, &len);
         buffer[n] = '\0';
-        printf("Client: %s\n", buffer);
-
-        printf("You: ");
-        fgets(buffer, BUFLEN, stdin);
-        sendto(sockfd, (const char *)buffer, strlen(buffer), 0, (const struct sockaddr *) &client_addr, addr_len);
+        printf("\nClient: %s", buffer);
+        fflush(stdout);
     }
+    return NULL;
+}
+
+void* send_messages(void* arg) {
+    char buffer[BUFFER_SIZE];
+    while (1) {
+        printf("You: ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&cliaddr, len);
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in servaddr;
+    int port = atoi(argv[1]);
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(port);
+
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is running on port %d...\n", port);
+
+    pthread_t recv_thread, send_thread;
+    pthread_create(&recv_thread, NULL, receive_messages, NULL);
+    pthread_create(&send_thread, NULL, send_messages, NULL);
+
+    pthread_join(recv_thread, NULL);
+    pthread_join(send_thread, NULL);
+
     close(sockfd);
-    return 0;
+    return 0;
 }
